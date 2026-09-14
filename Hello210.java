e22f88c36d60e4e350486567b09971ef03d5fa80
gbguenth29 <gb29guen@siena.edu>
2026-09-13 23:01:38 -0400

    Added name and extra printout.

--- Hello210.java
+++ Hello210.java
@@ -2,12 +2,15 @@
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author Grace Guenthner
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("Catskill");
+        System.out.println("Bob's Diner");
+        System.out.println("Breakfast Burrito");
     }
 }
